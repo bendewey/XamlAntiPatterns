ac916ca2b24f9afb76d1680ccd7fde7042092e41
Ben Dewey <bdewey01@hotmail.com>
2014-04-14 23:13:22 -0400

    Initial Commit

---
+++ XamlAntiPatterns/1-InlineStyles/InlineStyles1.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace XamlAntiPatterns._1_InlineStyles
+{
+    /// <summary>
+    /// Interaction logic for InlineStyles1.xaml
+    /// </summary>
+    public partial class InlineStyles1 : Page
+    {
+        public InlineStyles1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ XamlAntiPatterns/Menu.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace XamlAntiPatterns
+{
+    /// <summary>
+    /// Interaction logic for Menu.xaml
+    /// </summary>
+    public partial class Menu : Page
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void Navigate_Click(object sender, RoutedEventArgs e)
+        {
+            var uri = (string)((FrameworkElement)sender).Tag;
+            NavigationService.Navigate(new Uri(uri, UriKind.Relative));
+        }
+
+        private void ScrollViewer_PreviewMouseWheel(object sender, System.Windows.Input.MouseWheelEventArgs e)
+        {
+            ScrollViewer scrollviewer = sender as ScrollViewer;
+            if (e.Delta > 0)
+                scrollviewer.LineLeft();
+            else
+                scrollviewer.LineRight();
+            e.Handled = true;
+        }
+    }
+}

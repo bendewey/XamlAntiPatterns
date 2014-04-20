bd96e646eb33de2b01e312a891709b873c1df24b
Ben Dewey <bdewey01@hotmail.com>
2014-04-19 21:39:57 -0400

    Added Responsive

---
+++ XamlAntiPatterns/7-Responsive/Responsive.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace XamlAntiPatterns._7_Responsive
+{
+    /// <summary>
+    /// Interaction logic for Responsive.xaml
+    /// </summary>
+    public partial class Responsive : Page
+    {
+        public Responsive()
+        {
+            InitializeComponent();
+        }
+    }
+}

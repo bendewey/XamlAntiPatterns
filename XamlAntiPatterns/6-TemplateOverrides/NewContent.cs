a6770b5ae465f82cd61baf6b72b501fb190d899b
Ben Dewey <bdewey01@hotmail.com>
2014-04-19 21:14:23 -0400

    Create Template/Custom Controls

---
+++ XamlAntiPatterns/6-TemplateOverrides/NewContent.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace XamlAntiPatterns._6_TemplateOverrides
+{
+    public class NewContent : ContentControl
+    {
+        public NewContent()
+        {
+            DefaultStyleKey = typeof(NewContent);
+        }
+        
+        public string EarmarkText
+        {
+            get { return (string)GetValue(EarmarkTextProperty); }
+            set { SetValue(EarmarkTextProperty, value); }
+        }
+
+        public static readonly DependencyProperty EarmarkTextProperty =
+            DependencyProperty.Register("EarmarkText", typeof(string), 
+                    typeof(NewContent), new PropertyMetadata(null));
+    }
+}
---
+++ XamlAntiPatterns/6-TemplateOverrides/TemplateOverrides.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace XamlAntiPatterns._6_TemplateOverrides
+{
+    /// <summary>
+    /// Interaction logic for Icons.xaml
+    /// </summary>
+    public partial class TemplateOverrides : Page
+    {
+        public TemplateOverrides()
+        {
+            InitializeComponent();
+        }
+    }
+}
